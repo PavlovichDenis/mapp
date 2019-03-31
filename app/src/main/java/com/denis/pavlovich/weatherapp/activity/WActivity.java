ee52df197f95b43ea6196f5a4cb4f41435267ac1
DenisPavlovich <pavlovich_denis@mail.ru>
2019-03-31 20:34:53 +0300

    M2L1 Домашнее задание 1. Модуль 2

--- app/src/main/java/com/denis/pavlovich/weatherapp/activity/WActivity.java
+++ app/src/main/java/com/denis/pavlovich/weatherapp/activity/WActivity.java
@@ -2,22 +2,51 @@ package com.denis.pavlovich.weatherapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.denis.pavlovich.weatherapp.R;
 import com.denis.pavlovich.weatherapp.utils.WConstants;
 
 public class WActivity extends WAbstractActivityWithThemeSupport {
 
+    private final NavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    onOptionsItemSelected(menuItem);
+                    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+            };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(getApplicationTheme());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_w);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        View view = findViewById(R.id.wetherText);
+        registerForContextMenu(view);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
@@ -25,6 +54,16 @@ public class WActivity extends WAbstractActivityWithThemeSupport {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
 
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        return onOptionsItemSelected(item);
     }
 
     private void showAbout() {
@@ -36,7 +75,6 @@ public class WActivity extends WAbstractActivityWithThemeSupport {
         Intent intent = new Intent(WActivity.this, WActivitySettings.class);
         startActivity(intent);
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
